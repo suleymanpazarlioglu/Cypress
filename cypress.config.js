c7d6f47cd76ce6b81c3d707ce14b449e5ddc71a1
Suleyman Pazarlioglu <s.pazarlioglu1987@hotmail.com>
2023-05-09 15:09:15 +0300

    second

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl:"https://practice.cydeo.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+      },
   },
 });
--- cypress/e2e/1-getting-started/testStructure.cy.js
+++ cypress/e2e/1-getting-started/testStructure.cy.js
@@ -1,25 +1,21 @@
 /// <reference types="cypress" />
 
-describe("Context : My First Tests",()=>{
-    before(()=>{
+describe("Context : My First Tests", () => {
+    before(() => {
         //runs once before all test cases in this describe block,like beforeClass in TestNG
     })
-    beforeEach(()=> {
+    beforeEach(() => {
         //run before each test case,beforeMethod in TestNG
         cy.clearCookies();
     })
-    after(()=>{
+    after(() => {
         //similar to afterClass in TestNG,runs once after all tests finished
     })
-    afterEach(()=>{
+    afterEach(() => {
         //similar to afterMethod in TestNG,run after each test case
     })
-    
-    it("Openning a web application",()=>{
-        cy.visit("https://practice.cydeo.com/");
-        cy.get(':nth-child(7) > a').click();
-        cy.get('#box1').click();
-        cy.get('#box2').click();
-        
+
+    it("Openning a web application", () => {
+        cy.visit("/registration_form");
     })
 })
